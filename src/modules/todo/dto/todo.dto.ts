2054998e7412a348f00a0a6b321b39e357e2dbae
wallycoding <wallysonbreno.dev@gmail.com>
2023-10-12 10:54:55 -0300

    feat: todo dto

---
+++ src/modules/todo/dto/todo.dto.ts
@@ -0,0 +1,30 @@
+// http://localhost:3000/api
+
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, Length, ValidateIf } from 'class-validator';
+
+export class TodoWriteDTO {
+  @ApiProperty()
+  @IsNotEmpty()
+  @Length(3, 1000)
+  content: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  @ApiProperty()
+  done: boolean;
+}
+
+export class TodoUpdateDTO {
+  @ApiProperty()
+  @ValidateIf((o) => typeof o.done === 'undefined')
+  @IsNotEmpty()
+  @Length(3, 1000)
+  content: string;
+
+  @ApiProperty()
+  @ValidateIf((o) => typeof o.content === 'undefined')
+  @IsNotEmpty()
+  @IsBoolean()
+  done: boolean;
+}
