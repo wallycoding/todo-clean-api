690a5f1c55a4330b6f7f3512cc1c3ac63a6552d5
wallycoding <wallysonbreno.dev@gmail.com>
2023-10-13 09:48:03 -0300

    feat: add todo service in todo module

--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TodoService } from './services/todo/todo.service';
 
-@Module({})
+@Module({
+  providers: [TodoService]
+})
 export class TodoModule {}
